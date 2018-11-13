2e3a6c41bab9c7cbc8c447384d27440ec662ca79
Ajay Reddy <ajay.reddy@edureka.co>
2018-11-13 23:19:32 +0530

    wave animation applied

--- thinking_animation/thinking_animation.js
+++ thinking_animation/thinking_animation.js
@@ -1,6 +1,6 @@
     currentDivIndex=0;
     loaderTextLenght=0;
-    timeout=150;
+    timeout=100;
  $(document).ready(function(){
 
         divElement="<span class='loader_text'></span>";
@@ -17,20 +17,39 @@
         {
             loaderDivs.get(i).innerHTML=loaderText[i];
         }
-        animateText();
+
+        $(".loader_text").get(0).classList.add("stepup-one");
+        currentDivIndex=0;
+        setTimeout(animateText(),timeout);
 
     });
 
     function animateText()
     {
         currentDivIndex=currentDivIndex%loaderTextLenght;
-        $(".loader_text").get(currentDivIndex).classList.add("takeup");
-        if(currentDivIndex-1==-1)
+        $(".loader_text").get(currentDivIndex).classList.add("steup-two");
+        if(currentDivIndex+1<loaderTextLenght)
         {
-            $(".loader_text").get(loaderTextLenght-1).classList.remove("takeup");
+            $(".loader_text").get(currentDivIndex+1).classList.add("steup-one");
         }
         else{
-            $(".loader_text").get(currentDivIndex-1).classList.remove("takeup");
+            $(".loader_text").get(0).classList.add("steup-one");
+        }
+
+        if(currentDivIndex>=2)
+        {
+            $(".loader_text").get(currentDivIndex-1).classList.remove("steup-two");
+            $(".loader_text").get(currentDivIndex-2).classList.remove("steup-one");
+        }
+        else if(currentDivIndex==1)
+        {
+            $(".loader_text").get(0).classList.remove("steup-two");
+            $(".loader_text").get(loaderTextLenght-1).classList.remove("steup-one");
+        }
+        else if(currentDivIndex==0)
+        {
+            $(".loader_text").get(loaderTextLenght-1).classList.remove("steup-two");
+            $(".loader_text").get(loaderTextLenght-2).classList.remove("steup-one");
         }
         setTimeout(animateText,timeout);
         currentDivIndex++;
